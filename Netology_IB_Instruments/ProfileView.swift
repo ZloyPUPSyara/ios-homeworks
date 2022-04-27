d3e1443f379f9bfaf353536acb2850eea29ed0b2
ZloyPUPSyara <megusta0075@gmail.com>
2022-04-27 18:15:36 +0300

    My first app

---
+++ Netology_IB_Instruments/ProfileView.swift
@@ -0,0 +1,19 @@
+//
+//  ProfileView.swift
+//  Netology_IB_Instruments
+//
+//  Created by Юлия on 27.04.2022.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+
+    @IBOutlet weak var avatarImageView: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var dateOfBirthLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var descriptionTextView: UITextView!
+    
+    
+}
